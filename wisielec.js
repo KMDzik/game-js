28949563d27280f4507e643b580691c836bdeefe
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-02 16:00:59 +0200

    html set up

--- wisielec.js
+++ wisielec.js
@@ -1,1 +1,15 @@
-var
+var passwords = [
+  "dzik",
+  "happy",
+  "developerem",
+  "działa"
+];
+
+var password = passwords[Math.floor(Math.random() * passwords.length)];
+
+var answers = [];
+for (i = 0; i < password.length; i++) {
+  answers[i] = "_";
+};
+
+var restCharacters = password.length;
