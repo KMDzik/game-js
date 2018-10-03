b45dbda1ee3b9082ec28d356df426fdd554b28e8
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-03 14:56:11 +0200

    I GAVE UP! I WILL MAKE A SNAKE GAME

--- wisielec.js
+++ wisielec.js
@@ -8,9 +8,16 @@ var password1 = "";
 for (i = 0; i < password.length; i++) {
 
   if (password.charAt(i) === " ") {
-  return password1 = password1 + " ";
+  password1 = password1 + " ";
   }
   else {
-     return password1 = password1 + "-";
+     password1 = password1 + "-";
   }
 }
+
+
+function setPassword()
+{
+	document.querySelector(".user-field").innerHTML = password1;
+}
+
