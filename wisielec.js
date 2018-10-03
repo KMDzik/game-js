c30d3bb24748825ddfdbf1710a846a28131f9f7c
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-03 13:56:15 +0200

    creating cells in user-field

--- wisielec.js
+++ wisielec.js
@@ -1,13 +1,17 @@
-var password = [
-  "dzik jest dziki",
-  "be happy",
-  "frontend",
-  "u mnie działa"
-];
-
+var password = "dzik jest dziki";
 password = password.toUpperCase();
 
-var length = password.length;
 var howManyMistakes = 0;
 
 var password1 = "";
+
+for (i = 0; i < password.length; i++) {
+
+  if (password.charAt(i) === " ") {
+  return password1 = password1 + " ";
+  }
+  else {
+     return password1 = password1 + "-";
+  }
+}
+
